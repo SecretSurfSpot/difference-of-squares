392be0ed6bb7a3284712f4e414e78389327131c9
Andrew Gibbs <asg1968@hotmail.co.uk>
2019-03-06 12:19:10 +0000

    feat: added simplest code to pass first test (testSquareOfSumUpToOne)

--- src/main/java/DifferenceOfSquaresCalculator.java
+++ src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,15 +1,15 @@
 class DifferenceOfSquaresCalculator {
 
     int computeSquareOfSumTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      return 1;
     }
 
     int computeSumOfSquaresTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      return 1;
     }
 
     int computeDifferenceOfSquares(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      return 1;
     }
 
 }
